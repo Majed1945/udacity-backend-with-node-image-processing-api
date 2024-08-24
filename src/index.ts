82d259a712dd519d35f7786d591091575b6f5c83
Majed1945 <majedbiazid@gmail.com>
2024-08-24 19:11:07 +0300

    Server setup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import routes from './routes/indexRoutes';
+import { logger } from './middlewares/loggerMiddleware';
+
 const app = express();
 const port = 3000;
 
+app.use(logger);
+app.use('/api', routes);
+
 app.listen(port, () => {
-  console.log(`server started at localhost ${port}`);
+  console.log(`Server started at http://localhost:${port}`);
 });
---
+++ src/routes/imageRoutes.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+const imageRoutes = Router();
+
+imageRoutes.get('/resize', (req, res) => {
+  //TODO - implement image controller here
+});
+
+export default imageRoutes;
